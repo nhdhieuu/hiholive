dbd2bd77f5c514a6f7b86a2eca00e1499cfcfa23
nhdhieuu <22520439@gm.uit.edu.vn>
2024-12-23 15:19:09 +0700

    Add: isLogging in Home

--- src/layouts/components/TopBar.tsx
+++ src/layouts/components/TopBar.tsx
@@ -1,10 +1,26 @@
 import { Button } from "@/components/ui/button.tsx";
 import { Mail } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar.tsx";
 
 export const TopBar = () => {
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
+  useEffect(() => {
+    const auth = localStorage.getItem("token");
+    console.log("auth", auth);
+    if (auth && auth.length > 0) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
   return (
     <div className="sticky top-0 z-50 border">
       <div className="flex justify-between items-center p-5 bg-background">
@@ -22,20 +38,30 @@ export const TopBar = () => {
           <button className={"hover:bg-accent p-1 rounded-md"}>
             <Mail size={32} />
           </button>
-          <Button
-            onClick={() => {
-              navigate("/login");
-            }}
-          >
-            Log in
-          </Button>
-          <Button
-            onClick={() => {
-              navigate("/signup");
-            }}
-          >
-            Sign Up
-          </Button>
+          {isLoggedIn ? (
+            <Avatar className={"cursor-pointer"}>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          ) : (
+            <div>
+              <Button
+                onClick={() => {
+                  navigate("/login");
+                }}
+              >
+                Log in
+              </Button>
+              <Button
+                onClick={() => {
+                  navigate("/signup");
+                }}
+              >
+                Sign Up
+              </Button>
+            </div>
+          )}
+
           {/*<Avatar className={"cursor-pointer"}>
                         <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
                         <AvatarFallback>CN</AvatarFallback>
